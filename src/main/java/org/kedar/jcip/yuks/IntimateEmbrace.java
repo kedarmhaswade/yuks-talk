c714458344dac9646c48a56e5af35a95bc17d774
Kedar Mhaswade <kedar@uber.com>
2018-03-28 04:18:22 -0700

    rename to IntimateEmbrace

--- src/main/java/org/kedar/jcip/yuks/IntimateEmbrace.java
+++ src/main/java/org/kedar/jcip/yuks/IntimateEmbrace.java
@@ -9,27 +9,25 @@ import java.util.concurrent.Future;
 
 /**
  * <p> Quiz based on JCiP listing 8.1</p>
- * <ol>
- *     <li> Does this class compile/run? What problem do you see?</li>
- *     <li> What is the minimal code chat <i>will</i> cause this program to surely deadlock?</li>
- * </ol>
+ * <p> What does this program do?</p>
  */
-public class DependentTaskDeadlock {
+public class IntimateEmbrace {
     private static final Random rand = new Random();
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService exec = Executors.newSingleThreadExecutor();
+        ExecutorService exec = Executors.newFixedThreadPool(1);
         runIt(exec);
     }
 
     private static void runIt(ExecutorService exec) throws ExecutionException, InterruptedException {
         Future<Integer> future = exec.submit(new BusyRandomNumberGenerator(exec));
         Integer val = future.get();
-        System.out.println("in: " + Thread.currentThread().getName() + " I got: " + val);
+//        System.out.println("in: " + Thread.currentThread().getName() + " I got: " + val);
+        System.out.println(val);
         exec.shutdown();
     }
 
-    /** A very busy task that generates and returns a random number */
+    /** A busy task that generates and returns a random number */
     private static final class BusyRandomNumberGenerator implements Callable<Integer> {
         private final ExecutorService exec;
         BusyRandomNumberGenerator(ExecutorService exec) {
@@ -39,12 +37,11 @@ public class DependentTaskDeadlock {
         public Integer call() throws Exception {
             Future<Integer> x = exec.submit(() -> {
                 int num = rand.nextInt(1000);
-                System.out.println("in: " + Thread.currentThread().getName() + " I am going to return: " + num);
+//                System.out.println("in: " + Thread.currentThread().getName() + " I am going to return: " + num);
                 return num;
             });
             Integer num = x.get() + 10;
-            System.out.println(
-                "in: " + Thread.currentThread().getName() + " I am going to return: " + num);
+//            System.out.println("in: " + Thread.currentThread().getName() + " I am going to return: " + num);
             return num;
         }
     }
